ebd0e0bdcaddd7dadea1894cb230e35b5f90fe20
Kirill Kvashonin <etozhkirill@yandex.ru>
2018-11-04 15:42:04 +0300

    add shaker sort

--- examples/shaker_sort/main.c
+++ examples/shaker_sort/main.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include "../../cArray.h"
-#define SIZE 10
 
 void swap(int *a, int *b);
 
 int main(int argc, char const *argv[]) {
-  // int const SIZE = 10;
+  int const SIZE = 10;
   int *arr = getRandomArray(SIZE, 0, 100);
+  int maxI = SIZE - 1, minI = 0;
+
   printf("Array before sorting: \n");
   printArr(arr, SIZE);
 
   for (int i = 0; i < SIZE; i++) {
-    for (int j = 0; j < SIZE - 1; j++) {
-      if (arr[j] > arr[j + 1]) swap(&arr[j], &arr[j + 1]);
+    if (i % 2 == 0) {
+      for (int j = minI; j < maxI; j++) {
+        if (arr[j] > arr[j + 1]) swap(&arr[j], &arr[j + 1]);
+      }
+      maxI -= 1;
+    } else {
+      for (int j = maxI; j > minI; j--) {
+        if (arr[j] < arr[j - 1]) swap(&arr[j], &arr[j - 1]);
+      }
+      minI += 1;
     }
   }
 
